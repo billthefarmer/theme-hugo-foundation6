c0e1429ab4edf1bec939d485814713fcb9c02cea
Tony Ko <htko89@gmail.com>
2016-12-13 00:11:38 -0500

    Move: Build modules into dedicated folder.

--- bin/gulpfile.babel.js
+++ bin/gulpfile.babel.js
@@ -3,7 +3,6 @@
 import plugins  from 'gulp-load-plugins';
 import yargs    from 'yargs';
 import gulp     from 'gulp';
-import rimraf   from 'rimraf';
 import yaml     from 'js-yaml';
 import fs       from 'fs';
 
@@ -13,7 +12,7 @@ const $ = plugins();
 // Check for --production flag
 const PRODUCTION = !!(yargs.argv.production);
 
-// Load settings from settings.yml
+// Load settings from config.yml
 const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
 
 function loadConfig() {
@@ -29,29 +28,21 @@ gulp.task('build',
 gulp.task('default',
   gulp.series('build');
 
-// Delete the "dist" folder
-// This happens every time a build starts
-// function clean(done) {
-//   rimraf(PATHS.dist, done);
-// }
-
 // Compile Sass into CSS
 // In production, the CSS is compressed
 function sass() {
-  return gulp.src('src/assets/scss/app.scss')
+  return gulp.src(PATHS.src + '/scss/app.scss')
     .pipe($.sourcemaps.init())
     .pipe($.sass({
       includePaths: PATHS.sass
-    })
+      })
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: COMPATIBILITY
-    }))
-    // Comment in the pipe below to run UnCSS in production
-    //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
+      }))
     .pipe($.if(PRODUCTION, $.cssnano()))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest(PATHS.dist + '/assets/css'))
+    .pipe(gulp.dest(PATHS.dist + '/css'))
     .pipe(browser.reload({ stream: true }));
 }
 
@@ -64,7 +55,7 @@ function javascript() {
     .pipe($.concat('app.js'))
     .pipe($.if(PRODUCTION, $.uglify()
       .on('error', e => { console.log(e); })
-    ))
+      ))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest(PATHS.dist + '/assets/js'));
+    .pipe(gulp.dest(PATHS.dist + '/js'));
 }
